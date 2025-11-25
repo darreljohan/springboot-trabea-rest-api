500cd250652c95d0b99b28682cf7574cb3cd1271
darreljohan <darreljohan162@gmail.com>
2025-11-25 09:52:55 +0700

    Init Project

---
+++ src/main/java/com/iglo/trabea/TrabeaApplication.java
@@ -0,0 +1,13 @@
+package com.iglo.trabea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iglo/trabea/TrabeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iglo.trabea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
